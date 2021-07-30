a8a27e6008ed906c33d39125599f422471b9f435
Christopher Zhang <christophertzhang@gmail.com>
2021-07-29 22:37:32 -0400

    functions

--- src/index.js
+++ src/index.js
@@ -4,14 +4,27 @@ import './index.css';
 
 // ========================================
 
-const two_clubs = <img src="images/2C.png"  alt="two clubs" height = "106" width = "69"></img>
+const two_clubs = <img src="images/2C.png"  alt="two clubs" height = "106" width = "69"></img>;
 
 class Square extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {card: {props.value}};
+  }
+
+  switch = () => {
+    if (this.state.card == null){
+      this.setState({card: two_clubs});
+    } else {
+      this.setState({card: null});
+    }
+  }
+
   render() {
     return (
-      <div className="square">
-        {this.props.value}
+      <div className="square" onClick = {this.switch}>
+        {this.state.card}
       </div>
     );
   }
@@ -23,11 +36,16 @@ class Board extends React.Component {
     this.state = {button: "Click me"};
   }
 
-  renderSquare(i) {
-    return <Square value = {i} />;
+  renderSquare(show) {
+    if (show == true) {
+      return <Square />;
+    } else {
+      return <Square />;
+    }
+
   }
 
-  hi(){
+  hi = () => {
     const button = "Clicked";
     this.setState({button: button});
   }
@@ -41,7 +59,7 @@ class Board extends React.Component {
           {this.state.button}
         </button>
         <div className="board-row">
-          {this.renderSquare(0)}
+          {this.renderSquare(true)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
